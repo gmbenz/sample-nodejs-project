09437db5568a5365f0c6b1ddfbb372258d57c559
Glenn Benz <glenn.benz@gmail.com>
2025-11-18 11:51:39 -0500

    Add some output

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function add(a, b) {
 }
 
 module.exports = { add };
+
+// If this file is executed directly, show a small demo including the Node version
+if (require.main === module) {
+  console.log('Node version:', process.version);
+  console.log('Demo: add(1,2) =', add(1, 2));
+}
